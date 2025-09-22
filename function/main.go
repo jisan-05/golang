8e8162671f383a16f4c061e6025dfb7d9bf1ad05
jisan-05 <zishantanvir05@gmail.com>
2025-09-22 20:44:00 +0600

    first commit

---
+++ function/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+	return sum
+}
+func getNumbers(num1 int, num2 int) (int,int)  {
+	sum := num1 + num2
+	mul := num1 * num2
+	return  sum , mul
+}
+
+func main() {
+	a := 10
+	b := 20
+	p,q := getNumbers(a,b)
+	fmt.Println(p)
+	fmt.Println(q)
+}
